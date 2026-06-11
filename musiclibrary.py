2246a6f3eb11a0df4f1154ca90ff41467e07a253
KODUR SURYA TEJA <kodursuryatejaa@gmail.com>
2026-06-11 15:41:17 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "Zee music south": "https://www.youtube.com/@zeemusicsouth/videos",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi _- oOR6wm0Vi-10siLiV5ePSPs9oF&index=21"
+}
